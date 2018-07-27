0fd818daf2f2c61efbfa5e9773a28597a053cbb4
bigdaze <andrewsday@icloud.com>
2018-07-27 03:21:41 -0500

    After Completing Code First in Chapter 4

---
+++ DAL/BookContext.cs
@@ -0,0 +1,29 @@
+﻿using ASP.NET_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+
+namespace ASP.NET_Project.DAL
+{
+    public class BookContext : DbContext
+    {
+        public BookContext() : base("BookContext")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
